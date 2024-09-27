4d8ae04c3d619e93097dc2b12290843ae65e6ed8
Nisreen Hisham <157918418+nhi-vuega@users.noreply.github.com>
2024-09-27 16:08:43 +0300

    fix webdriver

--- app.py
+++ app.py
@@ -256,15 +256,20 @@ def fetch_and_save_reviews(driver, url, before_cleaning_file):
 
     print(f"Reviews before cleaning saved to {before_cleaning_file}")
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Initialize WebDriver
-#service = Service(r'C:\Users\nancy\.wdm\drivers\chromedriver\win32\109.0.5414.74\chromedriver.exe')
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")  # Enable headless mode
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--start-maximized")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+try:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 if st.button("Search"):
     if query:
